8537ad56291f525a0c2f1ded3a866784e42d318d
Uzay Bardakçı <uzaybardakci.cme@gmail.com>
2023-08-29 12:44:45 +0300

    main first commit
    I uploaded my development to github

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        [SerializeField] Transform target;
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            UpdateAnimator();
+
+        }
+
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localeVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localeVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+    }
+}
