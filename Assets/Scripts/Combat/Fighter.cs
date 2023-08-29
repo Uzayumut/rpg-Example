82f97749bf9873221fbc533d78aa81bf0bc00ed9
Uzay Bardakçı <uzaybardakci.cme@gmail.com>
2023-08-29 20:09:38 +0300

    RPG-28 branch fixed

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,3 +1,4 @@
+using RPG.Movement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,37 @@ namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        public void Attack(CombatTarget target)
+        [SerializeField] float weaponRange = 2f;
+        Transform target;
+        private void Update()
         {
-            print("attack");
+            //bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
+            //print(isInRange);
+            if (target != null)
+            {
+                if(!(Vector3.Distance(transform.position, target.position) < weaponRange))
+                {
+                    GetComponent<Mover>().MoveTo(target.position);
+                    print("test");
+                }
+                else
+                {
+                    GetComponent<Mover>().Stop();
+                    //target = null;
+                    print("durmasý gerek");
+                }
+                
+            }
+            else
+            {
+                //GetComponent<Mover>().MoveTo(target.position);
+                print("test2");
+            }
+            
+        }
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
         }
         
     }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -10,12 +10,17 @@ namespace RPG.Movement
         // Start is called before the first frame update
         [SerializeField] Transform target;
 
-
+        NavMeshAgent navMeshAgent;
+        private void Start()
+        {
+            navMeshAgent= GetComponent<NavMeshAgent>();
+            navMeshAgent.isStopped = false;
+        }
 
         // Update is called once per frame
         void Update()
         {
-
+            //navMeshAgent.isStopped = false;
             UpdateAnimator();
 
         }
@@ -23,12 +28,17 @@ namespace RPG.Movement
 
         public void MoveTo(Vector3 destination)
         {
-            GetComponent<NavMeshAgent>().destination = destination;
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+        public void Stop()
+        {
+            navMeshAgent.isStopped= true;
         }
 
         private void UpdateAnimator()
         {
-            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 velocity = navMeshAgent.velocity;
             Vector3 localeVelocity = transform.InverseTransformDirection(velocity);
             float speed = localeVelocity.z;
             GetComponent<Animator>().SetFloat("forwardSpeed", speed);
