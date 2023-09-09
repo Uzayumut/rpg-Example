4b03200c24b22e22181af3c1ef013d3e59e9780d
Uzay Bardakçı <uzaybardakci.cme@gmail.com>
2023-09-09 16:26:54 +0300

    RPG-52 draw line gizmos
    Çizdiğimiz gizmoslar arasında bir yol belirlemek için hr bi sphere arasına line çizdik

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -11,8 +11,24 @@ namespace RPG.Control
         {
             for (int i = 0; i < transform.childCount; i++)
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position,wayPointGizmoRadius);
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(transform.GetChild(i).position, wayPointGizmoRadius);
+                Gizmos.DrawLine(GetWayPoint(i), GetWayPoint(j));
             }
+
+            
+        }
+        private int GetNextIndex(int i)
+        {
+            if (i + 1 >= transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+        private Vector3 GetWayPoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
